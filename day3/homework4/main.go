b468bc443dcb0905e3522be4dd4f422f8478ae7f
Khoi.LN <147135167+khoilen@users.noreply.github.com>
2024-11-17 14:10:33 +0700

    Feat: add homework day 3  (#2)
    * feat: add excercise day1
    
    * feat: add homework4
    
    * feat: remove update data folder
    
    * feat: update defer
    
    ---------
    
    Co-authored-by: khoinashtech <lenguyenkhoi2806@gmail.com>

---
+++ day3/homework4/main.go
@@ -0,0 +1,181 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Profile  string `json:"profile"`
+}
+
+const usersFilePath = "./data/users.json"
+
+func readUsers() []User {
+	var users []User
+
+	file, err := os.Open(usersFilePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return users
+		}
+		panic(err)
+	}
+
+	defer file.Close()
+
+	data, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(data, &users)
+	if err != nil {
+		panic(err)
+	}
+
+	return users
+}
+
+func saveUsers(users []User) {
+	file, err := os.Create(usersFilePath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	data, err := json.MarshalIndent(users, "", "  ")
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = file.Write(data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	router := gin.Default()
+	router.Use(gin.Logger())
+	router.Use(gin.Recovery())
+
+	if _, err := os.Stat("uploads"); os.IsNotExist(err) {
+		os.Mkdir("uploads", os.ModePerm)
+	}
+
+	if _, err := os.Stat("data"); os.IsNotExist(err) {
+		os.Mkdir("data", os.ModePerm)
+	}
+
+	router.POST("/register", func(c *gin.Context) {
+		var newUser User
+		if err := c.ShouldBindJSON(&newUser); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		users := readUsers()
+		for _, user := range users {
+			if user.Username == newUser.Username {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "User already exists"})
+				return
+			}
+		}
+
+		users = append(users, newUser)
+		saveUsers(users)
+		c.JSON(http.StatusCreated, gin.H{"message": "User registered successfully"})
+	})
+
+	router.POST("/login", func(c *gin.Context) {
+		var loginUser User
+
+		if err := c.ShouldBindJSON(&loginUser); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		users := readUsers()
+		for _, user := range users {
+			if user.Username == loginUser.Username && user.Password == loginUser.Password {
+				c.JSON(http.StatusOK, gin.H{"message": "Login successful"})
+				return
+			}
+		}
+
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid username or password"})
+	})
+
+	router.GET("/user/:username", func(c *gin.Context) {
+		username := c.Param("username")
+		users := readUsers()
+
+		for _, user := range users {
+			if user.Username == username {
+				c.JSON(http.StatusOK, user)
+				return
+			}
+		}
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+	})
+
+	router.POST("/user/:username", func(c *gin.Context) {
+		var updateUser User
+		if err := c.ShouldBindJSON(&updateUser); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		users := readUsers()
+		for i, user := range users {
+			if user.Username == updateUser.Username {
+				users[i].Username = updateUser.Username
+				users[i].Password = updateUser.Password
+				saveUsers((users))
+				c.JSON(http.StatusOK, gin.H{"message": "User information updated"})
+				return
+			}
+		}
+
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+	})
+
+	router.POST("/user/:username/profile", func(c *gin.Context) {
+		username := c.Param("username")
+
+		file, err := c.FormFile("profile")
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+
+		users := readUsers()
+
+		for i, user := range users {
+			if user.Username == username {
+				filename := filepath.Join("uploads", filepath.Base(file.Filename))
+				if err := c.SaveUploadedFile(file, filename); err != nil {
+					c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+					return
+				}
+				users[i].Profile = filename
+				saveUsers(users)
+				c.JSON(http.StatusOK, gin.H{"message": "Profile picture uploaded"})
+				return
+			}
+		}
+
+		c.JSON(http.StatusNotFound, gin.H{"error": "User not found"})
+	})
+
+	router.Run(":8080")
+}
